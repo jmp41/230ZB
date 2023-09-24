bb5ebc171756fa695a3fbb8abae4a20b50dfc84e
jmp41 <jianming_pan@berkeley.edu>
2023-09-23 22:14:37 -0700

    modify traning

--- make_dataset.py
+++ make_dataset.py
@@ -15,6 +15,9 @@ from qlib.utils import get_callable_kwargs
 from qlib.utils import flatten_dict
 from dataclasses import dataclass
 import os
+from qlib.data import D
+from qlib.model.riskmodel import StructuredCovEstimator
+
 
 #make argparse
 parser = argparse.ArgumentParser()
@@ -100,7 +103,43 @@ def main(args):
     train.to_pickle(f'/home/jianming/PONet/program/project/FactorVAE/data/train_sp500_QLIB_{args.use_qlib}_NORM_{args.normalize}_CHAR_{args.select_feature}_LEN_{args.seq_len}.pkl')
     valid.to_pickle(f'/home/jianming/PONet/program/project/FactorVAE/data/valid_sp500_QLIB_{args.use_qlib}_NORM_{args.normalize}_CHAR_{args.select_feature}_LEN_{args.seq_len}.pkl')
     test.to_pickle(f'/home/jianming/PONet/program/project/FactorVAE/data/test_sp500_QLIB_{args.use_qlib}_NORM_{args.normalize}_CHAR_{args.select_feature}_LEN_{args.seq_len}.pkl')
-    
+
+def prepare_data(riskdata_root="./riskdata", T=240, start_time="2010-12-01"):
+    universe = D.features(D.instruments("sp500"), ["$close"], start_time=start_time).swaplevel().sort_index()
+
+    price_all = (
+        D.features(D.instruments("all"), ["$close"], start_time=start_time).squeeze().unstack(level="instrument")
+    )
+
+    # StructuredCovEstimator is a statistical risk model
+    riskmodel = StructuredCovEstimator()
+
+    for i in range(T - 1, len(price_all)):
+        date = price_all.index[i]
+        ref_date = price_all.index[i - T + 1]
+        print(date)
+
+        codes = universe.loc[date].index
+        price = price_all.loc[ref_date:date, codes]
+
+        # calculate return and remove extreme return
+        ret = price.pct_change()
+        ret.clip(ret.quantile(0.025), ret.quantile(0.975), axis=1, inplace=True)
+
+        # run risk model
+        F, cov_b, var_u = riskmodel.predict(ret, is_price=False, return_decomposed_components=True)
+
+        # save risk data
+        root = riskdata_root + "/" + date.strftime("%Y%m%d")
+        os.makedirs(root, exist_ok=True)
+
+        pd.DataFrame(F, index=codes).to_pickle(root + "/factor_exp.pkl")
+        pd.DataFrame(cov_b).to_pickle(root + "/factor_cov.pkl")
+        # for specific_risk we follow the convention to save volatility
+        pd.Series(np.sqrt(var_u), index=codes).to_pickle(root + "/specific_risk.pkl")
     
 if __name__ == "__main__":
-    main(args)
+    # main(args)
+    provider_uri = "~/.qlib/qlib_data/us_data"  # target_dir
+    qlib.init(provider_uri=provider_uri, region=REG_US)
+    prepare_data()
